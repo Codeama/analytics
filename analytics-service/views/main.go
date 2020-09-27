b34d85e7c78bd0cbe525cf6c61c353780ae779e6
Bukola Jimoh <bukolajimoh@economist.com>
2020-09-27 03:05:00 +0100

    extract SNS publish function

--- analytics-service/views/main.go
+++ analytics-service/views/main.go
@@ -5,14 +5,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sns"
-	"github.com/codeama/analytics/analytics-service/views/tags"
+	"github.com/codeama/analytics/analytics-service/views/publish"
+	"github.com/codeama/analytics/analytics-service/views/tag"
 )
 
 // IncomingData represents data event received
@@ -22,8 +19,6 @@ type IncomingData struct {
 	PreviousPage string `json:"previousPage"`
 	CurrentPage  string `json:"currentPage"`
 }
-
-type AttributeValue map[string]*sns.MessageAttributeValue
 
 func handleRequest(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// todo Send raw events to SNS
@@ -38,48 +33,20 @@ func handleRequest(ctx context.Context, request events.APIGatewayWebsocketProxyR
 
 	fmt.Println("Incoming event:", string(result))
 
-	var forwardData tags.ViewData
+	var forwardData tag.ViewData
 	forwardData.ArticleID = data.ArticleID
 	forwardData.ArticleTitle = data.ArticleTitle
 	forwardData.PreviousPage = data.PreviousPage
 	forwardData.CurrentPage = data.CurrentPage
 	forwardData.ConnectionID = request.RequestContext.ConnectionID
 
-	// // Process data
-	translatedData, _ := tags.TranslateData(forwardData)
+	taggedData, _ := tag.TranslateData(forwardData)
 
-	// // Marshal data
-	processedData, _ := json.Marshal(translatedData)
+	processedData, _ := json.Marshal(taggedData)
 
 	fmt.Println("Processed data:", string(processedData))
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("eu-west-1"),
-	})
-
-	if err != nil {
-		fmt.Println("NewSession error:", err)
-		return events.APIGatewayProxyResponse{}, err
-	}
-	// tODO extract SNS function
-	client := sns.New(sess)
-	input := &sns.PublishInput{
-		Message: aws.String(string(processedData)),
-		MessageAttributes: AttributeValue{
-			"event_type": {
-				DataType:    aws.String("String"),
-				StringValue: aws.String("post_views"),
-			},
-		},
-		TopicArn: aws.String(os.Getenv("TOPIC_ARN")),
-	}
-
-	_, err = client.Publish(input)
-	if err != nil {
-		fmt.Println("Publish error:", err)
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	// fmt.Println(result)
+	// publish to SNS
+	publish.SendEvent(string(taggedData.Event), string(processedData))
 
 	return events.APIGatewayProxyResponse{
 		Body:       string(fmt.Sprintf("New lambda: User %s connected!", request.RequestContext.ConnectionID)),
---
+++ analytics-service/views/publish/send-sns.go
@@ -0,0 +1,52 @@
+// Package publish adds appropriate event type and sends to SNS
+package publish
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
+type AttributeValue map[string]*sns.MessageAttributeValue
+
+func publish(eventType string, data string) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+	client := sns.New(sess)
+	input := &sns.PublishInput{
+		Message: aws.String(data),
+		MessageAttributes: AttributeValue{
+			"event_type": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(eventType),
+			},
+		},
+		TopicArn: aws.String(os.Getenv("TOPIC_ARN")),
+	}
+
+	_, err = client.Publish(input)
+	if err != nil {
+		fmt.Println("Publish error:", err)
+		return
+	}
+}
+
+// SendEvent publishes events to SNS
+func SendEvent(event string, data string) {
+	switch event {
+	case "post_view":
+		publish("post_view", data)
+	case "contact_view":
+		publish("profile_view", data)
+	case "about_view":
+		publish("profile_view", data)
+	default:
+		fmt.Println("What is this data?", data)
+		// publish("raw_data", data)
+	}
+
+}
