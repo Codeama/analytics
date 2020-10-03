f9069f580f35007f1eae6b8c2bc7291dfb6a29a3
Bukola Jimoh <bukolajimoh@economist.com>
2020-10-03 08:16:08 +0100

    update build-deploy scripts

--- build_package.sh
+++ build_package.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-# TODO Package mgmt malarkey >:<
-# TODO take lambda-directory-name aas arg and run a different function for local devvelopment
+# TODO Go package mgmt malarkey >.<
+
+# with args ---> for local development
+# if [[ -n "$1" ]]; then
+#     dirname=$1
+#     cd "$dirname" || exit
+#     GOOS=linux go build -o dist/main main.go
+#     zip main.zip main
+#     #  upload/update function code
+# fi
+
 service_handlers="
 analytics-service/default
 analytics-service/views
 analytics-service/post-handler
+analytics-service/profile-handler
 "
 
 # Build and package Go Lambda binaries
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd infra || exit
+npm i
+cdk deploy
