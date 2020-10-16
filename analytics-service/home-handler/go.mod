df89acb276d0840c4df46b8996e61cc536888a1d
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2020-10-16 07:10:31 +0100

    scaffold subscriber lambdas

---
+++ analytics-service/home-handler/go.mod
@@ -0,0 +1,5 @@
+module github.com/codeama/analytics/analytics-service/home-handler
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.19.1
---
+++ analytics-service/home-handler/main.go
@@ -0,0 +1,25 @@
+// Homepage view queue subscriber
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// type ResponseData struct {
+// 	ConnectionID string `json:"connectionId"`
+// }
+
+func handleQueueMessage(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+	}
+	return nil
+}
+
+func main() {
+	lambda.Start(handleQueueMessage)
+}
