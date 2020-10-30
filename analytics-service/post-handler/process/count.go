f5e5db25014bce61f94a962f719738ca6935d454
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2020-10-30 06:38:27 +0000

    add logic for unique views and update testdata

--- analytics-service/post-handler/process/count.go
+++ analytics-service/post-handler/process/count.go
@@ -25,44 +25,71 @@ type ProcessedEvent struct {
 	TotalViews   int // includes a count of non-unique views
 }
 
+func isUnique(previousPage string, currentPage string) bool {
+	// UNIQUE: if currentPage is not a / and previousPage is not null
+	// HomePage is "/"
+	if currentPage != "/" && previousPage != "null" {
+		return true
+	}
+	return false
+}
+
 // CountViews totals the number of views for each article
-// It filters for each unique article by implementing a set using Go map
+// It filters each article by implementing a set using Go map
 // and totals them each one being a view (1)
 // Employs a map reduce paradigm
 // TODO count unique views
 // TODO create a function to extract map values and return an array of items to send to the database
 func CountViews(sqsEvent events.SQSEvent) (map[string]ProcessedEvent, error) {
 	var data incomingEvent
-	var views = make(map[string]int)
+	var totalViews = make(map[string]int)
+	var uniqueViews = make(map[string]int)
 	var mappedArt = make(map[string]ProcessedEvent)
 	for _, message := range sqsEvent.Records {
 		// serialise to Go struct
 		if err := json.Unmarshal([]byte(message.Body), &data); err != nil {
 			return nil, err
 		}
-		_, exists := views[data.ArticleID]
-		if exists {
-			_, article := mappedArt[data.ArticleID]
-			if article {
-				delete(mappedArt, data.ArticleID)
+		// checks current article has a view value
+		_, hasViews := totalViews[data.ArticleID]
+		// _, hasUniqueViews := uniqueViews[data.ArticleID]
+		// checks article is already in map (it should be)
+		// then updates it (deletes and replaces with article item
+		// with the latest view count)
+		_, exists := mappedArt[data.ArticleID]
+		if hasViews && exists {
+			delete(mappedArt, data.ArticleID)
+
+			// process unique views
+			unique := isUnique(data.PreviousPage, data.CurrentPage)
+			if unique {
+				uniqueViews[data.ArticleID]++
 			}
-			views[data.ArticleID]++
+			// process all views
+			totalViews[data.ArticleID]++
+
 			processed := ProcessedEvent{
 				ArticleID:    data.ArticleID,
 				ArticleTitle: data.ArticleTitle,
 				ConnectionID: data.ConnectionID,
-				UniqueViews:  views[data.ArticleID],
-				TotalViews:   views[data.ArticleID],
+				UniqueViews:  uniqueViews[data.ArticleID],
+				TotalViews:   totalViews[data.ArticleID],
 			}
 			mappedArt[data.ArticleID] = processed
 		} else {
-			views[data.ArticleID] = 1
+			unique := isUnique(data.PreviousPage, data.CurrentPage)
+			if unique {
+				uniqueViews[data.ArticleID] = 1
+			} else {
+				uniqueViews[data.ArticleID] = 0
+			}
+			totalViews[data.ArticleID] = 1
 			processed := ProcessedEvent{
 				ArticleID:    data.ArticleID,
 				ArticleTitle: data.ArticleTitle,
 				ConnectionID: data.ConnectionID,
-				UniqueViews:  views[data.ArticleID],
-				TotalViews:   views[data.ArticleID],
+				UniqueViews:  uniqueViews[data.ArticleID],
+				TotalViews:   totalViews[data.ArticleID],
 			}
 			mappedArt[data.ArticleID] = processed
 		}
