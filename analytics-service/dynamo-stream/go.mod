a682c3058d6f6a363e511418b4cc3e2098dfeadf
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2020-11-08 09:22:13 +0000

    implement stream handler lambda

--- analytics-service/dynamo-stream/go.mod
+++ analytics-service/dynamo-stream/go.mod
@@ -2,4 +2,7 @@ module github.com/codeama/analytics/analytics-service/dynamo-stream
 
 go 1.13
 
-require github.com/aws/aws-lambda-go v1.20.0
+require (
+	github.com/aws/aws-lambda-go v1.20.0
+	github.com/aws/aws-sdk-go v1.35.23
+)
--- analytics-service/dynamo-stream/main.go
+++ analytics-service/dynamo-stream/main.go
@@ -1,15 +1,21 @@
-// Homepage view queue subscriber
+// This Lambda reads data from DynamoDB stream and writes to
+// another table (for publishing views)
 package main
 
 import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/codeama/analytics/analytics-service/dynamo-stream/stream"
 )
 
-func handleQueueMessage(ctx context.Context) error {
-	fmt.Println("Hello, DynamoDB streams!")
+func handleQueueMessage(ctx context.Context, streamData events.DynamoDBEvent) error {
+
+	if err := stream.UpdateTable(streamData); err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
 	return nil
 }
 
