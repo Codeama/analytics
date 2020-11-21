a18368cab2e874b774802d4de5535e2ce49c52d3
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2020-11-21 20:17:20 +0000

    implement data out logic for articles

--- analytics-service/views-stream/go.mod
+++ analytics-service/views-stream/go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/aws/aws-lambda-go v1.20.0
-	github.com/aws/aws-sdk-go v1.35.19
+	github.com/aws/aws-sdk-go v1.35.33
 	github.com/stretchr/testify v1.6.1
 )
--- analytics-service/views-stream/main.go
+++ analytics-service/views-stream/main.go
@@ -18,7 +18,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/codeama/analytics/analytics-service/views-stream/process"
 	"github.com/codeama/analytics/analytics-service/views-stream/publish"
+	"github.com/codeama/analytics/analytics-service/views-stream/store"
 )
+
+type response struct {
+	ArticleViews int `json:"uniqueViews"`
+}
 
 func getSession() (*sns.SNS, error) {
 	sess, err := session.NewSession(&aws.Config{
@@ -35,13 +40,13 @@ func handleRequest(ctx context.Context, request events.APIGatewayWebsocketProxyR
 	fmt.Println("Incoming event:", string(request.Body))
 	var data process.IncomingData
 	if err := json.Unmarshal([]byte(request.Body), &data); err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+		return events.APIGatewayProxyResponse{StatusCode: 400}, fmt.Errorf("Error: %v", err)
 	}
 
 	// validate
 	validated, err := process.ValidateData(data, request.RequestContext.ConnectionID)
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+		return events.APIGatewayProxyResponse{StatusCode: 400}, fmt.Errorf("Error: %v", err)
 	}
 
 	// filter
@@ -59,11 +64,26 @@ func handleRequest(ctx context.Context, request events.APIGatewayWebsocketProxyR
 
 	// publish to SNS
 	if err := tag.SendEvent(session, taggedData); err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 422}, err
+		return events.APIGatewayProxyResponse{StatusCode: 422}, fmt.Errorf("SendEvent Error: %v", err)
 	}
 
+	views := store.ArticleViews{}
+	// check incoming data is an article
+	if data.ArticleID != "" {
+		// read article stats
+		views, err = store.GetArticleViews(data.ArticleID)
+		if err != nil {
+			return events.APIGatewayProxyResponse{StatusCode: 400}, fmt.Errorf("ReadTable Error: %v", err)
+		}
+
+	}
+
+	stats, err := json.Marshal(views)
+
 	return events.APIGatewayProxyResponse{
-		Body:       string(fmt.Sprintf("New lambda: User %s connected!", request.RequestContext.ConnectionID)),
+		// Mehh when you finish a project and realise you don't need websockets >.<
+		// But I learned a lot...
+		Body:       string(stats),
 		StatusCode: 200,
 	}, nil
 }
