d9f70bf7479e1a4b70a8d99291caba0ae6536878
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2020-12-29 08:56:35 +0000

    update and use deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd infra || exit
 npm i
 
 # Deploys the CDK stacks as they're named in infra.ts
-cdk deploy "${NAMESPACE}AnalyticsDataStore"
-cdk deploy "${NAMESPACE}AnalyticsApi"
+cdk deploy "${NAMESPACE}AnalyticsDataStore" --require-approval never
+cdk deploy "${NAMESPACE}AnalyticsApi" --require-approval never
 now=$(date +"%x - %T")
 echo "Last run : $now"
